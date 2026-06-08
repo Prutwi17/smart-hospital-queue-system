10689dac49025fae536920ffbdce8e28e92ddf88
Pruthibiraj Swain <pruthwiraj314@gmail.com>
2026-06-08 14:48:40 +0530

    Create User entity

---
+++ src/main/java/com/hospital/entity/User.java
@@ -0,0 +1,31 @@
+package com.hospital.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    private boolean active = true;
+}
