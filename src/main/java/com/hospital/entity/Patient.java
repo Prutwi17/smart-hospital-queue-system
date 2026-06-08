c8108a1434334e1433654cfa5198301d262738df
Pruthibiraj Swain <pruthwiraj314@gmail.com>
2026-06-08 14:52:12 +0530

    Create Patient entity

--- src/main/java/com/hospital/entity/Patient.java
+++ src/main/java/com/hospital/entity/Patient.java
@@ -1,26 +1,32 @@
 package com.hospital.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Table(name = "patients")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Patient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private Integer age;
 
-    private int age;
+    private String gender;
+
+    private String phone;
+
+    private String bloodGroup;
+
+    private String address;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
 }
